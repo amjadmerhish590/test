1b24dc3e42623ffc3e76cbdb5d8c0b08be2ef6bf
amjadmerhish <amjadmerhish@gmail.com>
2025-09-20 16:50:23 +0300

    first commit

---
+++ lib/utils/function.dart
@@ -0,0 +1,13 @@
+import 'package:url_launcher/url_launcher.dart';
+
+
+Future<void> launchCustomUrl (context, String? url) async {
+  if (url != null) {
+    Uri uri = Uri.parse(url);
+    if (await canLaunchUrl(uri)) {
+      await launchUrl(uri);
+    } else {
+      throw  "Can not launch $url" ;
+    }
+  }
+}
